a870c2f7b547aaec9c2c0e630d2fbaffb1f3394b
Клименков Дмитрий <dekosik2014@gmail.com>
2019-05-15 16:32:20 +0900

    Стартовые изменения

--- mpets.mobi.bot/Form1.cs
+++ mpets.mobi.bot/Form1.cs
@@ -4,17 +4,27 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using mpets.bot.Libs;
 
 namespace mpets.mobi.bot
 {
     public partial class Form1 : Form
     {
+        private static INIManager settings = new INIManager(AppDomain.CurrentDomain.BaseDirectory + "settings.ini");
+        private static HttpClient client = new HttpClient();
+        private static Random random = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void log(string text, bool show_times, Color color = new Color())
+        {
+
         }
     }
 }
---
+++ mpets.mobi.bot/libs/INIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace mpets.bot.Libs
+{
+    class INIManager
+    {
+        public INIManager(string aPath)
+        {
+            path = aPath;
+        }
+
+        public INIManager() : this("") { }
+
+        public string get(string aSection, string aKey)
+        {
+            StringBuilder buffer = new StringBuilder(SIZE);
+            GetPrivateString(aSection, aKey, null, buffer, SIZE, path);
+            return buffer.ToString();
+        }
+
+        public void write(string aSection, string aKey, string aValue)
+        {
+            WritePrivateString(aSection, aKey, aValue, path);
+        }
+
+        public string Path { get { return path; } set { path = value; } }
+
+        private const int SIZE = 1024;
+        private string path = null;
+
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateString(string section, string key, string def, StringBuilder buffer, int size, string path);
+
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern int WritePrivateString(string section, string key, string str, string path);
+    }
+}
